c940dfe2bc5b98a8b5023f4ad404bef2ec427b89
Krishna Teja <thotakrishnateja444@gmail.com>
2026-03-03 00:04:59 +0530

    Time: 1 ms (14.46%) | Memory: 19.5 MB (22.59%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,9 +1,22 @@
 class Solution:
+    def qsort(self,l,low,high):
+        if low >= high :
+            return
+        p = l[low]
+        high_p = low + 1
+        for i in range(low+1,high+1):
+            if l[i] < p :
+                l[i] , l[high_p] = l[high_p] , l[i]
+                high_p += 1
+        l[low] , l[high_p-1] = l[high_p-1] , l[low]
+        self.qsort(l,low,high_p - 2)
+        self.qsort(l,high_p,high)
+
     def sortColors(self, nums: List[int]) -> None:
+        self.qsort(nums,0,len(nums)-1)
+        return nums
+        
+
        
-        for i in range(len(nums)):
-            for j in range(i,len(nums)):
-                if nums[i] > nums[j] :
-                    nums[i] , nums[j] = nums[j] , nums[i]
         
         
