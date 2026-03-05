8c4f66de07fbc855a971c9086282de8de9ce9f28
Krishna Teja <thotakrishnateja444@gmail.com>
2026-03-05 23:33:53 +0530

    Time: 10 ms (17.31%) | Memory: 21.1 MB (62.55%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -3,13 +3,12 @@ class Solution:
         
         if len(nums) == 1 :
             return nums[0]
-        l = []
+    
         nums.sort()
-        for i in range(1,len(nums)):
-            if nums[i-1] == nums[i] :
-                l.append(nums[i])
-        k = sum(nums) - 2*sum(l)
-        return k
+        for i in range(1,len(nums),2):
+            if nums[i-1] != nums[i] :
+                return nums[i-1]
+        return nums[len(nums)-1]
                 
          
         
