4c58e9e88e802dffec065efa6d13a3d18bd4e97c
Krishna Teja <thotakrishnateja444@gmail.com>
2026-03-09 23:06:45 +0530

    Time: 15 ms (42.80%) | Memory: 19.5 MB (72.58%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -3,19 +3,19 @@ class Solution:
         if len(s) != len(t) :
             return False
         d1 = {}
-        d2 = {}
         for i in range(len(s)):
             if s[i] not in d1 :
                 d1[s[i]] = 1
             else :
                 d1[s[i]] += 1
-            if t[i] not in d2 :
-                d2[t[i]] = 1
+            if t[i] not in d1 :
+                d1[t[i]] = -1
             else :
-                d2[t[i]] += 1
-        if d1 == d2 :
-            return True
-        return False
+                d1[t[i]] -= 1
+        for i in s :
+            if d1[i] != 0 :
+                return False
+        return True
             
 
 
