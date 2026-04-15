b83a4ae07fddbe3f8a387710f3d865420614c3de
Krishna Teja <thotakrishnateja444@gmail.com>
2026-04-15 11:51:34 +0530

    Time: 7 ms (45.74%) | Memory: 20.2 MB (47.31%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,17 +1,32 @@
 class Solution:
     def setZeroes(self, m: List[List[int]]) -> None:
-        rows = set()
-        columns = set()
+        row_zero = False
+        column_zero = False
+        for j in range(len(m[0])):
+            if m[0][j] == 0 :
+                row_zero = True
+            
         for i in range(len(m)):
-            for j in range(len(m[0])):
+            if m[i][0] == 0 :
+                column_zero = True
+        for i in range(1,len(m)):
+            for j in range(1,len(m[0])):
                 if m[i][j] == 0 :
-                    rows.add(i)
-                    columns.add(j)
-        for i in rows :
-            for j in range(len(m[0])):
-                m[i][j] = 0
-        for j in columns :
-            for k in range(len(m)):
-                m[k][j] = 0
+                    m[0][j] = 0
+                    m[i][0] = 0
         
-        
+        for i in range(1,len(m)):
+            if m[i][0] == 0 :
+                for j in range(len(m[0])):
+                    m[i][j] = 0
+        for j in range(1,len(m[0])):
+            if m[0][j] == 0 :
+                for i in range(len(m)):
+                    m[i][j] = 0
+        if row_zero :
+            for i in range(len(m[0])):
+                m[0][i] = 0
+        if column_zero :
+            for j in range(len(m)):
+                m[j][0] = 0
+        return m
