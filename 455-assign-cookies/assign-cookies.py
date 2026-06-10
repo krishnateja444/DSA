3edf245ebd5f9118cdb29d8a3dec6799e85adf30
Krishna Teja <thotakrishnateja444@gmail.com>
2026-06-10 14:35:49 +0530

    Time: 27 ms (31.09%) | Memory: 22 MB (69.40%) - LeetSync

--- 455-assign-cookies/assign-cookies.py
+++ 455-assign-cookies/assign-cookies.py
@@ -4,17 +4,9 @@ class Solution:
         s.sort()
         p1 = 0
         p2 = 0
-        count = 0
-        while True :
-            if p1 >= len(g):
-                break
-            if p2 >= len(s):
-                break
+        while p1 < len(g) and p2 < len(s) :
             if s[p2] >= g[p1]:
-                count += 1
                 p1 += 1
-                p2 += 1
-            else :
-                p2 += 1
-        return count
+            p2 += 1
+        return p1
         
