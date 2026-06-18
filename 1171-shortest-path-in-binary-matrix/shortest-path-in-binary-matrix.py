32144b520bbc362e8520d6d7ffa17dba9fc4b19c
Krishna Teja <thotakrishnateja444@gmail.com>
2026-06-18 23:11:35 +0530

    Time: 156 ms (59.84%) | Memory: 19.7 MB (91.52%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -3,22 +3,20 @@ class Solution:
         n = len(grid)
         if grid[0][0] != 0 or grid[n-1][n-1] != 0 :
             return -1
-        dist = [[float('inf')]*n for _ in range(n)]
-        dist[0][0] = 0
         from collections import deque
         q = deque()
         q.append((0,0,0)) ## (dist,row,col)
+        grid[0][0] = 1
         directions = [(-1, -1), (-1, 0), (-1, 1),(0, -1),(0, 1),(1, -1),  (1, 0),  (1, 1)]
         while q:
             d, r, c = q.popleft()
+            if r == n-1 and c == n-1:
+                return d + 1
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
                 if (0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == 0):
-                    if d + 1 < dist[nr][nc]:
-                        dist[nr][nc] = d + 1
-                        q.append((dist[nr][nc], nr, nc))
-        if dist[n-1][n-1] == float('inf'):
-            return -1
-        return dist[n-1][n-1] + 1
+                    grid[nr][nc] = 1
+                    q.append((d + 1, nr, nc))
+        return -1
 
         
