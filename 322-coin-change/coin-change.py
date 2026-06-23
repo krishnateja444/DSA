ea9c897b0216faf3cbac6838b53e848ba1415c39
Krishna Teja <thotakrishnateja444@gmail.com>
2026-06-23 17:33:08 +0530

    Time: 904 ms (20.23%) | Memory: 20.1 MB (33.30%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -3,20 +3,25 @@ class Solution:
         if amount == 0 :
             return 0
         n = len(coins)
-        dp = [[float('inf')]*(amount+1) for _ in range(n)]
-        for i in range(n):
-            dp[i][0] = 0
+        #dp = [[float('inf')]*(amount+1) for _ in range(n)]
+        prev = [float('inf')]*(amount+1)
+        #for i in range(n):
+            #dp[i][0] = 0
+        prev[0] = 0
         for i in range(amount+1):
             if i % coins[0] == 0 :
-                dp[0][i] = i // coins[0]
+                prev[i] = i // coins[0]
         for i in range(1,n):
+            curr = [float('inf')]*(amount+1)
+            prev[0] = curr[0] = 0
             for j in range(1,amount+1):
                 take = float('inf')
                 if j >= coins[i] :
-                    take = 1 + dp[i][j-coins[i]]
-                not_take = dp[i-1][j]
-                dp[i][j] = min(take,not_take)
-        ans = dp[n-1][amount]
+                    take = 1 + curr[j-coins[i]]
+                not_take = prev[j]
+                curr[j] = min(take,not_take)
+            prev = curr
+        ans = prev[amount]
         if ans == float('inf') :
             return -1
         return ans
