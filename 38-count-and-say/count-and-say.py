c95f0fd183da4acdc893efdb279afd0fe374e473
Krishna Teja <thotakrishnateja444@gmail.com>
2026-07-01 15:03:23 +0530

    Time: 5 ms (85.50%) | Memory: 19.4 MB (13.37%) - LeetSync

--- 38-count-and-say/count-and-say.py
+++ 38-count-and-say/count-and-say.py
@@ -15,10 +15,8 @@ class Solution:
                 else :
                     ans += str(c) + prev[i-1]
                     c = 1
-            if prev[len(prev)-1] == prev[len(prev)-2] :
-                ans += str(c) + prev[len(prev)-2]
-            else :
-                ans += str(1) + prev[len(prev)-1]
+            
+            ans += str(c) + prev[-1]
             return ans
         return cns(n)
 
