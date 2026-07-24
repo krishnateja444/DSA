7292131d0bc37ea34b1f43fdb4df7247ac869204
Krishna Teja <thotakrishnateja444@gmail.com>
2026-07-25 01:29:53 +0530

    Time: 60 ms (5.03%) | Memory: 20.4 MB (31.79%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isBipartite(self, adj: List[List[int]]) -> bool:
+        n = len(adj)
+        ans = [-1]*(n)
+        from collections import deque
+        for start in range(n):
+            if ans[start] != -1 :
+                continue
+            q = deque()
+            q.append((start,0))
+        #visited = [False]*(n)
+            while q :
+                node,c = q.popleft()
+                ans[node] = c
+            #visited[node] = True
+                for nei in adj[node]:
+                    if ans[nei] == c :
+                        return False
+                    if ans[nei] == -1 :
+                        q.append((nei,1 - c))
+        return True
